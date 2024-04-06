6e89c702214ab7ccbdbe449612af8164eacc2f11
Sanchita Lavande <126872499+SanchitaLavande1999@users.noreply.github.com>
2024-04-06 20:20:34 +0530

    Update test.py
    Added comments in program

--- GitHub-integration/test.py
+++ GitHub-integration/test.py
@@ -1,8 +1,15 @@
+#import request package
 import requests
+
+#request.get will fetch the details from provided url
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
+
+#To get details in json format
 complete_details = response.json()
+
 # Print the id of first element in list
 print(complete_details[0]["id"])
+
 # The whole web page is list
 # Create for loop to iterate through loop and get all user names
 for i in range(len(complete_details)):
